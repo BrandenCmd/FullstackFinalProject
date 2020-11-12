c77829261688c5acf653edf9413e6feac010ee46
BrandenCmd <branden@live.ca>
2020-11-12 00:36:15 -0600

    Displaying images inside activeadmin

--- app/admin/asteroids.rb
+++ app/admin/asteroids.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Asteroid do
   permit_params :name, :description, :cost, :asteroid_type_id, :image
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+    end
+    f.actions
+  end
 end
--- app/admin/stars.rb
+++ app/admin/stars.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Star do
   permit_params :name, :description, :cost, :star_type_id, :image
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+    end
+    f.actions
+  end
 end

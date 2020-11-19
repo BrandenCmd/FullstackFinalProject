e5e2b9913975043a959357f21e03a375b6ee3966
BrandenCmd <branden@live.ca>
2020-11-18 21:30:24 -0600

    Fixed partial and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,8 @@
 Rails.application.routes.draw do
-  get 'asteroid/index'
-  get 'asteroid/show'
-  get 'asteroid/alphabetized'
-  get 'asteroid/newest'
-  get 'asteroid/hightolow'
-  get 'asteroid/lowtohigh'
-  get 'star/index'
-  get 'star/show'
-  get 'star/alphabetized'
-  get 'star/newest'
-  get 'star/hightolow'
-  get 'star/lowtohigh'
-  get 'planet/index'
-  get 'planet/show'
-  get 'planet/alphabetized'
-  get 'planet/newest'
-  get 'planet/hightolow'
-  get 'planet/lowtohigh'
+  resources :planet, only: %i[index show alphabetized newest hightolow lowtohigh]
+  resources :star, only: %i[index show alphabetized newest hightolow lowtohigh]
+  resources :asteroid, only: %i[index show alphabetized newest hightolow lowtohigh]
   root to: "home#index"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

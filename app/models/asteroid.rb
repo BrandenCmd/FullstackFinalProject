e2f5b500060ecf749fdd00eda2fee4828773c9a8
BrandenCmd <branden@live.ca>
2020-11-18 23:24:34 -0600

    Added numericality validation

--- app/models/asteroid.rb
+++ app/models/asteroid.rb
@@ -3,5 +3,6 @@ class Asteroid < ApplicationRecord
 
   validates :name, :description, :cost, presence: true
   validates :name, uniqueness: true
+  validates :cost, numericality: true
   has_one_attached :image
 end
--- app/models/star.rb
+++ app/models/star.rb
@@ -3,5 +3,6 @@ class Star < ApplicationRecord
 
   validates :name, :description, :cost, presence: true
   validates :name, uniqueness: true
+  validates :cost, numericality: true
   has_one_attached :image
 end

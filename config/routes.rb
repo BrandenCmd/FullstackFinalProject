088df6bc11ded0c0779ef8653d9c59207487da4a
BrandenCmd <branden@live.ca>
2020-11-19 00:10:30 -0600

    Styled pagination and fixed image alt attr

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :planet, only: %i[index show alphabetized newest hightolow lowtohigh]
-  resources :star, only: %i[index show alphabetized newest hightolow lowtohigh]
-  resources :asteroid, only: %i[index show alphabetized newest hightolow lowtohigh]
+  resources :planet, only: %i[index show]
+  resources :star, only: %i[index show]
+  resources :asteroid, only: %i[index show]
   root to: "home#index"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)

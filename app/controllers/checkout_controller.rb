45a5b7c37e78c87a9017728a384b7ac4c294a411
BrandenCmd <branden@live.ca>
2020-12-06 04:38:00 -0600

    Stripe checkout, it works

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,6 +1,34 @@
 class CheckoutController < ApplicationController
   def create
-    # c
+    planets = Planet.find(params[:planet_ids])
+
+    if planets.nil?
+      redirect_to root_path
+      nil
+    end
+
+    line_items = planets.map do |planet|
+      {
+        name:        planet.name,
+        description: planet.description,
+        amount:      (planet.cost * 100).round(2).to_i,
+        currency:    "cad",
+        quantity:    1
+      }
+    end
+
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ["card"],
+      success_url:          checkout_success_url,
+      cancel_url:           checkout_cancel_url,
+      line_items:           [
+        line_items
+      ]
+    )
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   def success

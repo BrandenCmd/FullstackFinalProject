6c706f5886db35afd0f1150e63bdc78a1ba08454
BrandenCmd <branden@live.ca>
2020-12-07 16:02:46 -0600

    Checkout with taxes

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -17,12 +17,41 @@ class CheckoutController < ApplicationController
       }
     end
 
+    order_subtotal = 0
+
+    planets.each do |planet|
+      order_subtotal += (planet.cost * 100).round(2).to_i
+    end
+
+    user_province = Province.find(current_user.province_id)
+
     @session = Stripe::Checkout::Session.create(
       payment_method_types: ["card"],
       success_url:          checkout_success_url + "?session_id={CHECKOUT_SESSION_ID}",
       cancel_url:           checkout_cancel_url,
       line_items:           [
-        line_items
+        line_items,
+        {
+          name:        "GST",
+          description: "Goods and Services Tax",
+          amount:      (order_subtotal * user_province.gst_rate).round(2).to_i,
+          currency:    "cad",
+          quantity:    1
+        },
+        {
+          name:        "PST",
+          description: "Provincial Sales Tax",
+          amount:      (order_subtotal * user_province.pst_rate).round(2).to_i,
+          currency:    "cad",
+          quantity:    1
+        },
+        {
+          name:        "HST",
+          description: "Harmonized Sales Tax",
+          amount:      (order_subtotal * user_province.hst_rate).round(2).to_i,
+          currency:    "cad",
+          quantity:    1
+        }
       ]
     )
 
